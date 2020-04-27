05e8478e8aa9682bfb8480d2e72a81c270778f84
CZlotogura <cameron.zlotogura@gmail.com>
2020-04-26 21:03:23 -0400

    Added dokka to the build

--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -8,14 +8,23 @@ import android.widget.EditText
 
 const val EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE"
 
+/**
+ * Main Activity class that sends message to second activity
+ */
 class MainActivity : AppCompatActivity() {
-
+    /**
+     * Basic onCreate Function
+     */
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
-    /* Called when a user taps the Send button*/
+    /**
+     * Called when a user taps the Send button
+     *
+     * @param view View that calls the function
+     */
     fun sendMessage(view: View) {
         val editText = findViewById<EditText>(R.id.editText)
         val message = editText.text.toString()
